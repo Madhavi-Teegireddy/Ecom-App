daf35ce047cabc07284b35a428896a1922b75df0
madhavi-teegireddy <madhavitigireddi@gmail.com>
2023-03-12 21:27:14 +0530

    hashing

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,2 +1,54 @@
-export const registerController = () => {}
+import userModel from "../models/userModel"
 
+
+export const registerController = async(req,res) => {
+  try {
+    const { name, email, password, phone, address, answer } = req.body;
+    //validations
+    if (!name) {
+      return res.send({ error: "Name is Required" });
+    }
+    if (!email) {
+      return res.send({ message: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ message: "Password is Required" });
+    }
+    if (!phone) {
+      return res.send({ message: "Phone no is Required" });
+    }
+    if (!address) {
+      return res.send({ message: "Address is Required" });
+    }
+  
+    const exisitingUser = await userModel.findOne({ email });
+    //exisiting user
+    if (exisitingUser) {
+      return res.status(200).send({
+        success: false,
+        message: "Already Register please login",
+      });
+    }
+    const hashedPassword = await hashPassword(password);
+    const user = await new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hashedPassword
+    }).save();
+
+    res.status(201).send({
+      success: true,
+      message: "User Register Successfully",
+      user,
+    });
+    } catch(error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Already Registered Please login",
+      error,
+    });
+}
+}
