46c08ef4a3bdc416b49a4c413bfcfd6e3cf65897
madhavi-teegireddy <madhavitigireddi@gmail.com>
2023-03-13 19:54:14 +0530

    front end

--- frontend/src/Components/Layout/Footer.js
+++ frontend/src/Components/Layout/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div className='bg-dark text-light p-3'>
-        <h1 className='text-center'>Design & Developed by Madhavi</h1>
+    <div className="footer">
+      <h4 className="text-center">Design & Developed by ❤️ Madhavi</h4>
+      <p className="text-center mt-3">
+        <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
+        <Link to="/policy">Privacy Policy</Link>
+      </p>
     </div>
   )
 }
