9ade792959a5ef9b1fe9c28bd3bbbbc6b5de77be
madhavi-teegireddy <madhavitigireddi@gmail.com>
2023-03-14 21:21:32 +0530

    context

--- frontend/src/Pages/Admin/AdminDashboard.js
+++ frontend/src/Pages/Admin/AdminDashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "./../../components/Layout/Layout";
+import Layout from "../../Components/Layout/Layout";
 import { useAuth } from "../../Context/Auth"; 
 
 const AdminDashboard = () => {
