451b5af03ee686805785557a1bc5ba30eb354c8c
madhavi-teegireddy <madhavitigireddi@gmail.com>
2023-03-16 12:52:07 +0530

    backend

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 import formidable from "express-formidable";
-import { createProductController, deleteProductController, getProductController, getSingleProductController, productPhotoController } from "../controllers/productController.js";
+import { createProductController, deleteProductController, getProductController, getSingleProductController, productPhotoController, updateProductController } from "../controllers/productController.js";
 
 
 
@@ -20,7 +20,11 @@ router.get("/get-product/:slug", getSingleProductController);
 //get photo
 router.get("/product-photo/:pid", productPhotoController);
 
-//delete rproduct
+//delete product
 router.delete("/delete-product/:pid", deleteProductController);
+
+//routes
+router.put("/update-product/:pid", requireSignIn, isAdmin, formidable(), updateProductController
+);
 
 export default router
