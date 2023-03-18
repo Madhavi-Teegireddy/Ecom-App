bed154cd91cc04d731557b56215b994fd10e8b78
madhavi-teegireddy <madhavitigireddi@gmail.com>
2023-03-18 12:56:01 +0530

    ui for home page

--- frontend/src/Pages/ProductDetails.js
+++ frontend/src/Pages/ProductDetails.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
-import "../styles/ProductDetailsStyles.css";
+import "../Styles/ProductDetailsStyles.css";
+import Layout from "../Components/Layout/Layout";
 
 const ProductDetails = () => {
   const params = useParams();
