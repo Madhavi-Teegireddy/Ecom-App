d83f6a65c8b7f340a6705430ccb4387690e69fb8
madhavi-teegireddy <madhavitigireddi@gmail.com>
2023-03-22 21:07:20 +0530

    ecommmerce app

--- frontend/src/Components/Spinner.js
+++ frontend/src/Components/Spinner.js
@@ -20,7 +20,7 @@ const Spinner = ({ path = "login" }) => {
       <div
         className="d-flex flex-column justify-content-center align-items-center"
         style={{ height: "100vh" }}
-      >
+      > 
         <h1 className="Text-center">redirecting to you in {count} second </h1>
         <div className="spinner-border" role="status">
           <span className="visually-hidden">Loading...</span>
